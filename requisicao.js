408c0097f3a4b1a1fe8d129c66cddfc3f2d81b4c
giovaniocan <giovaniocan@gmail.com>
2024-10-22 20:54:02 -0300

    exemplo feito

---
+++ requisicao.js
@@ -0,0 +1,19 @@
+const URL = 'https://bz06fi12.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22historia%22%5D%7B%0A++historia%0A%7D';
+
+window.addEventListener('load', async () => {
+    const requisicao = await fetch(URL);
+
+    const resposta = await requisicao.json();
+
+    const historia = resposta.result;
+
+    historia.forEach(item => {
+        criarHistoria(item.historia)
+    });
+
+})
+
+function criarHistoria(historia){
+    const p = document.getElementById('historiaEmpresa');
+    p.innerText = historia;
+}
