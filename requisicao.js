480cbbd61df16e22ee0b5c15c0606d1a2315e2bd
giovaniocan <giovaniocan@gmail.com>
2024-10-22 21:44:46 -0300

    arrumado

--- requisicao.js
+++ requisicao.js
@@ -1,7 +1,10 @@
 const URL = 'https://bz06fi12.api.sanity.io/v2022-03-07/data/query/production?query=*%5B_type+%3D%3D+%22historia%22%5D%7B%0A++historia%0A%7D';
 
 window.addEventListener('load', async () => {
-    const requisicao = await fetch(URL);
+    const requisicao = await fetch(URL, {
+        method: "GET",
+    });
+   
 
     const resposta = await requisicao.json();
 
